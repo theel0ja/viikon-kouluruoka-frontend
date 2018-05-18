2d12b69b19b1d8f1141aea9e4a074953752bcd2f
Elias Ojala <git@eliasojala.me>
2018-05-19 02:59:49 +0300

    Show restaurant details

--- src/controllers/restaurants.controller.ts
+++ src/controllers/restaurants.controller.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import { NextFunction, Request, Response, Router } from "express";
 
 const router: Router = Router();
+
 /**
  * List all restaurants.
  */
@@ -12,7 +13,23 @@ router.get("/", (req: Request, res: Response, next: NextFunction) => {
     res.render("restaurants/list.twig", {
       title: "List of restaurants",
       restaurants: data,
-      restaurantsJson: JSON.stringify(data),
+      dataJson: JSON.stringify(data),
+    });
+  });
+});
+
+/**
+ * Show information of a restaurant.
+ */
+router.get("/:id", (req: Request, res: Response, next: NextFunction) => {
+  axios.get("http://localhost:3001/restaurants")
+  .then((response) => response.data)
+  .then((data) => data.find((x) => x.id === req.params.id))
+  .then((data) => {
+    res.render("restaurants/show.twig", {
+      title: data.name,
+      restaurant: data,
+      dataJson: JSON.stringify(data),
     });
   });
 });

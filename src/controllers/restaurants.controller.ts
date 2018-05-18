f7fb7103728da42f0fd8045110acaacbb162e4f2
Elias Ojala <git@eliasojala.me>
2018-05-19 02:52:44 +0300

    List of restaurants

--- src/controllers/restaurants.controller.ts
+++ src/controllers/restaurants.controller.ts
@@ -6,8 +6,14 @@ const router: Router = Router();
  * List all restaurants.
  */
 router.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.render("restaurants/list.twig", {
-    title: "List of restaurants",
+  axios.get("http://localhost:3001/restaurants")
+  .then((response) => response.data)
+  .then((data) => {
+    res.render("restaurants/list.twig", {
+      title: "List of restaurants",
+      restaurants: data,
+      restaurantsJson: JSON.stringify(data),
+    });
   });
 });
 

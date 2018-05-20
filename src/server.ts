650003e5d7e1b22ae3a9dbdf529d39c9aa0049c7
Elias Ojala <git@eliasojala.me>
2018-05-20 13:10:08 +0300

    Content-Security-Policy

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import twig from "twig";
+import lusca from "lusca";
 
 dotenv.config();
 
@@ -12,15 +13,36 @@ twig.extendFunction("getenv", (name: string) => {
   return process.env[name];
 });
 
+const production = process.env.NODE_ENV === "production";
+
 // Create a new express application instance
 const app: express.Application = express();
 app.disable("x-powered-by");
 
-// The port the express app will listen on
-const port: (string | number) = process.env.PORT || 3000;
+let cspReportUri: string;
+
+if (production) {
+  cspReportUri = process.env.REPORT_URI;
+} else {
+  cspReportUri = "/api/csp-report-dev";
+}
+
+app.use(lusca.csp({
+  /* tslint:disable:object-literal-sort-keys */
+  policy: {
+      "default-src": "'none'",
+      "style-src": "https://cdnjs.cloudflare.com",
+      "script-src": "'unsafe-inline' https://cdnjs.cloudflare.com https://cdn.theel0ja.info",
+      "report-uri": cspReportUri,
+  },
+  /* tslint:enable:object-literal-sort-keys */
+}));
 
 // Mount the RestaurantController at the /restaurants route
 app.use("/restaurants", RestaurantController);
+
+// The port the express app will listen on
+const port: (string | number) = process.env.PORT || 3000;
 
 // Serve the application at the given port
 app.listen(port, () => {

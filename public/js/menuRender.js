ce8847480852565078ad0da670d7c87bd4c5c96e
Elias Ojala <git@eliasojala.me>
2018-05-20 19:01:34 +0300

    menuRender first prototype

--- public/js/menuRender.js
+++ public/js/menuRender.js
@@ -1,3 +1,14 @@
-function menuRender(data) {
-  console.log(data);
+// eslint-disable-next-line no-unused-vars
+function menuRender (data) {
+  const menuContainer = document.getElementById("menuContainer");
+
+
+  let card = document.createElement("div");
+  card.classList.add("card");
+
+  card.innerHTML = `<pre>${JSON.stringify(data, "", 2)}</pre>`;
+
+  menuContainer.appendChild(card);
+
+  // console.log(data);
 }

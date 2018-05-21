37ecd8d4c66bd98c3598144a7396433d9f5a8ac2
Elias Ojala <git@eliasojala.me>
2018-05-21 15:29:23 +0300

    tslint

--- src/viikonKouluruokaSites.ts
+++ src/viikonKouluruokaSites.ts
@@ -1,9 +1,9 @@
-interface ViikonKouluruokaSites {
+interface IViikonKouluruokaSites {
   name: string; // Such as "Tampere"
   url: string;  // Such as "https://tampere.kouluruoka.eliasojala.me/"
 }
 
-const viikonKouluruokaSites: ViikonKouluruokaSites[] = [
+const viikonKouluruokaSites: IViikonKouluruokaSites[] = [
   {
     name: "Tampere",
     url: "https://tampere.kouluruoka.eliasojala.me/",

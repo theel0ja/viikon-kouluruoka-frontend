23376f063d39ebe7eaa1579362ffc3eade9f6c8a
Elias Ojala <git@eliasojala.me>
2018-05-23 16:51:52 +0300

    Matomo

--- src/viikonKouluruokaSites.ts
+++ src/viikonKouluruokaSites.ts
@@ -1,15 +1,18 @@
 interface IViikonKouluruokaSites {
   name: string; // Such as "Tampere"
+  id: string;   // Such as "tampere"
   url: string;  // Such as "https://tampere.kouluruoka.eliasojala.me/"
 }
 
 const viikonKouluruokaSites: IViikonKouluruokaSites[] = [
   {
     name: "Turku",
+    id: "turku",
     url: "https://turku.kouluruoka.eliasojala.me/",
   },
   {
     name: "Tampere",
+    id: "tampere",
     url: "https://tampere.kouluruoka.eliasojala.me/",
   },
 ];

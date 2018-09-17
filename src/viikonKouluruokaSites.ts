d51a898725a19865ba94083991ea2e2ff2474fcf
Elias Ojala <git@eliasojala.me>
2018-09-17 15:04:26 +0300

    Added api_url to viikonKouluruokaSites.ts

--- src/viikonKouluruokaSites.ts
+++ src/viikonKouluruokaSites.ts
@@ -1,7 +1,8 @@
 interface IViikonKouluruokaSites {
-  name: string; // Such as "Tampere"
-  id: string;   // Such as "tampere"
-  url: string;  // Such as "https://tampere.kouluruoka.eliasojala.me/"
+  name: string;    // Such as "Tampere"
+  id: string;      // Such as "tampere"
+  url: string;     // Such as "https://tampere.kouluruoka.eliasojala.me/"
+  api_url: string; // Such as "https://turku.kouluruoka-api.theel0ja.info"
 }
 
 const viikonKouluruokaSites: IViikonKouluruokaSites[] = [
@@ -9,11 +10,13 @@ const viikonKouluruokaSites: IViikonKouluruokaSites[] = [
     name: "Turku",
     id: "turku",
     url: "https://turku.kouluruoka.eliasojala.me/",
+    api_url: "https://turku.kouluruoka-api.theel0ja.info",
   },
   {
     name: "Tampere",
     id: "tampere",
     url: "https://tampere.kouluruoka.eliasojala.me/",
+    api_url: "https://tampere.kouluruoka-api.theel0ja.info",
   },
 ];
 
